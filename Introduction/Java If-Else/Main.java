f3fe5dbbe91da9030ab96adcd6b18713000268d1
Tomaszewski Maciej <tegor7pl@gmail.com>
2022-04-20 02:17:39 +0200

    Update Main.java

--- Introduction/Java If-Else/Main.java
+++ Introduction/Java If-Else/Main.java
@@ -19,16 +19,17 @@ public class Main {
         scanner.close();
 
         if (N % 2 == 1) {
-            System.out.println("Weird");
+            System.out.println("Weird");            //checking whether number is odd, if so then pritn Weird
         }
-        if (N % 2 == 0 && N >= 2 && N <= 5) {
+        if (N % 2 == 0 && N >= 2 && N <= 5) {       //checking whether number is even, greater or equal 2 and smaller than 5, if so then print Not Weird
             System.out.println("Not Weird");
         }
-        if (N % 2 == 0 && N >= 6 && N <= 20) {
+        if (N % 2 == 0 && N >= 6 && N <= 20) {      //checking whether number is even, greater/equal 6 and smaller than 20, if so then print Weird
             System.out.println("Weird");
         }
         if (N % 2 == 0 && N > 20) {
-            System.out.println("Not Weird");
+            System.out.println("Not Weird");        //checking whether number is even and greater than 20, if so then print Not Weird
+
         }
 
     }
