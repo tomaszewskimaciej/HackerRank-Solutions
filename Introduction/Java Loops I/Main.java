0aa8dd81e234a3d7283e212ea76dc1b6e1c2fe54
Tomaszewski Maciej <tegor7pl@gmail.com>
2022-04-21 16:23:49 +0200

    6/13

---
+++ Introduction/Java Loops I/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        for(int a=1; a<=10; a++){
+            System.out.println(N +" x " + a +" = " + N*a);
+        }
+    }
+}
