622fc8d76fdbe8f74b3332741afe4040ad5edb33
Tomaszewski Maciej <tegor7pl@gmail.com>
2022-04-21 17:41:49 +0200

    13/13

---
+++ Introduction/Java Currency Formatter/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        Locale indiaL = new Locale("en", "IN");
+        NumberFormat india = NumberFormat.getCurrencyInstance(indiaL);
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
+
