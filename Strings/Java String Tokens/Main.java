d85c4d1f04d7a208a98e711075657880fe141d56
Tomaszewski Maciej <tegor7pl@gmail.com>
2022-04-21 18:21:03 +0200

    6/11

---
+++ Strings/Java String Tokens/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        scan.close();
+        s = s.trim();
+        if (s.length() == 0) {
+            System.out.println(0);
+        } else {
+            String[] strings = s.split("['!?,._@ ]+");
+            System.out.println(strings.length);
+            for (String abc : strings)
+                System.out.println(abc);
+        }
+    }
+}
