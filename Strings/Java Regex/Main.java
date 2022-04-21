4db68084818b930564ec42ed4279569231428146
Tomaszewski Maciej <tegor7pl@gmail.com>
2022-04-21 18:33:38 +0200

    8/11

---
+++ Strings/Java Regex/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+    String pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+}
+
